43bfcb80f83c625f596185f6958d51fba48b028d
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-02-25 23:51:00 +0000

    movie controller has updated formating/refractoring

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -47,34 +47,34 @@ class MoviesController < ApplicationController
   def edit
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where(id: the_id)
 
     @the_movie = matching_movies.first
 
-    render({ :template => "movies/edit" })
+    render template: "movies/edit"
   end
 
   def update
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where(id: the_id).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to movie_url(the_movie), notice: "Movie updated successfully."
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to movie_url(the_movie),alert: "Movie failed to update successfully."
     end
   end
 
   def destroy
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where(id: the_id).first
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
