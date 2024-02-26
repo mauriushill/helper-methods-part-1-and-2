992ab89901045a46b6d0fdb7c2e02a99680f4ba3
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2024-02-26 21:24:54 +0000

    label tags and text area tags are updated.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,8 +23,8 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new
-    @movie.title = params.fetch("query_title")
-    @movie.description = params.fetch("query_description")
+    @movie.title = params.fetch("title")
+    @movie.description = params.fetch("description")
 
     if @movie.valid?
       @movie.save
@@ -42,8 +42,8 @@ class MoviesController < ApplicationController
     movie = Movie.find(params.fetch(:id))
 
 
-    movie.title = params.fetch("query_title")
-    movie.description = params.fetch("query_description")
+    movie.title = params.fetch("title")
+    movie.description = params.fetch("description")
 
     if movie.valid?
       movie.save
